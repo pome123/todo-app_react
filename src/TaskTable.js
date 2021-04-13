b477b13700bd2e3e44fbe84e91100e164505ca5b
fujine <fujine@sria.co.jp>
2021-04-13 19:25:31 +0900

    タスクの追加処理を構築。

--- src/TaskTable.js
+++ src/TaskTable.js
@@ -1,38 +1,38 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-import TaskRow from './TaskRow';
+// import TaskRow from './TaskRow';
 
 
-function TaskTable() {
-  const [tasks, setTask] = useState([
-    {
-      id: "rfiho6voxf",
-      name: 'Walk the dog',
-      complete: false,
-    },
-    {
-      id: "yxfpdj9fbj",
-      name: 'Wash the car',
-      complete: false,
-    },
-    {
-      id: "oscutvkozp",
-      name: 'Go to the gym',
-      complete: true,
-    }
-  ]);
+// function TaskTable() {
+//   const [tasks, setTask] = useState([
+//     {
+//       id: "rfiho6voxf",
+//       name: 'Walk the dog',
+//       complete: false,
+//     },
+//     {
+//       id: "yxfpdj9fbj",
+//       name: 'Wash the car',
+//       complete: false,
+//     },
+//     {
+//       id: "oscutvkozp",
+//       name: 'Go to the gym',
+//       complete: true,
+//     }
+//   ]);
 
-  return (
-    <table>
-      <tbody>
-        {
-          tasks.map(task => (
-            <TaskRow key={task.id} name={task.name} />
-          ))
-        }
-      </tbody>
-    </table>
-  );
-}
+//   return (
+//     <table>
+//       <tbody>
+//         {
+//           tasks.map(task => (
+//             <TaskRow key={task.id} name={task.name} />
+//           ))
+//         }
+//       </tbody>
+//     </table>
+//   );
+// }
 
-export default TaskTable;
+// export default TaskTable;
--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,74 @@
+import React, { useState } from 'react';
+
 import Title from './Title';
-import AddTaskBar from './AddTaskBar';
-import TaskTable from './TaskTable';
+// import AddTaskBar from './AddTaskBar';
+// import TaskTable from './TaskTable';
 import TaskRow from './TaskRow';
 
 function Todo() {
+  const [tasks, setTask] = useState([
+    {
+      id: "rfiho6voxf",
+      name: 'Walk the dog',
+      complete: false,
+    },
+    {
+      id: "yxfpdj9fbj",
+      name: 'Wash the car',
+      complete: false,
+    },
+    {
+      id: "oscutvkozp",
+      name: 'Go to the gym',
+      complete: true,
+    }
+  ]);
+  const [text, setText] = useState('');
+
+  const handleChange = function(e) {
+    setText(e.target.value);
+  }
+
+
+  function generateId() {
+    return Math.random().toString(36).substring(2);
+  }
+
+  const handleAddTask = function(e) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      // console.log('hoge');
+
+      setTask([
+        ...tasks,
+        {
+          id: generateId(),
+          name: text,
+          complete: false,
+        }
+      ]);
+
+      setText('');
+    }
+  }
+
   return (
     <div className="Todo">
       <Title />
-      <AddTaskBar />
-      <TaskTable>
-        {/* <TaskRow /> */}
-      </TaskTable>
+      {/* <AddTaskBar /> */}
+      <form>
+        {/* <input type="text" value={text} onChange={handleChange} /> */}
+        <input type="text" value={text} onChange={handleChange} onKeyPress={handleAddTask} />
+      </form>
+      <table>
+        <tbody>
+          {
+            tasks.map(task => (
+              <TaskRow key={task.id} name={task.name} />
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
