e36e5d66140d7e426ed20143d68638735c67f082
fujine <fujine@sria.co.jp>
2021-04-13 13:44:26 +0900

    初期データを追加。

--- src/TaskTable.js
+++ src/TaskTable.js
@@ -1,9 +1,40 @@
-import TaskRow from './TaskRow';
+import React, { useState } from 'react';
+
+// import TaskRow from './TaskRow';
+import Button from './Button';
+
+
 function TaskTable(props) {
+  const [tasks, setTask] = useState([
+    {
+      id: "rfiho6voxf",
+      name: 'Walk the dog',
+      complete: false,
+    },
+    {
+      id: "yxfpdj9fbj",
+      name: 'Wash the car',
+      complete: false,
+    },
+    {
+      id: "oscutvkozp",
+      name: 'Go to the gym',
+      complete: true,
+    }
+  ]);
+
   return (
     <table>
       <tbody>
-        <TaskRow task="Walk the dog" />
+        {tasks.map(task => (
+          <tr>
+            <td>
+              <input className="task" type="text" value={task.name} disabled />
+              <Button value="Edit" />
+              <Button value="Delete" />
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
